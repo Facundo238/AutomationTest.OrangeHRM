3512443c2e76bd9a6681bd0d723f64e51c740d7b
faq12 <faq1228@gmail.com>
2026-05-14 02:41:21 -0300

    refactor: merge Chrome/Edge wiring into single BrowserTests.cs

--- AutomationTest.OrangeHRM/Tests/BrowserTests.cs
+++ AutomationTest.OrangeHRM/Tests/BrowserTests.cs
@@ -1,11 +1,17 @@
 using OrangeHRM.AutomationTests.Fixtures;
 using Xunit.Abstractions;
 
-namespace OrangeHRM.AutomationTests.Tests.Browsers
+namespace OrangeHRM.AutomationTests.Tests
 {
     [CollectionDefinition("Chrome")]
     public class ChromeCollection : ICollectionFixture<ChromeAuthFixture> { }
 
+    [CollectionDefinition("Edge")]
+    public class EdgeCollection : ICollectionFixture<EdgeAuthFixture> { }
+
     [Collection("Chrome")] [Trait("Browser", "Chrome")] public class ChromeEmployeeTests(ChromeAuthFixture f, ITestOutputHelper o) : ABMEmployeeTest(f, o) { }
     [Collection("Chrome")] [Trait("Browser", "Chrome")] public class ChromeMyInfoTests(ChromeAuthFixture f) : MyInfoTest(f) { }
+
+    [Collection("Edge")] [Trait("Browser", "Edge")] public class EdgeEmployeeTests(EdgeAuthFixture f, ITestOutputHelper o) : ABMEmployeeTest(f, o) { }
+    [Collection("Edge")] [Trait("Browser", "Edge")] public class EdgeMyInfoTests(EdgeAuthFixture f) : MyInfoTest(f) { }
 }
